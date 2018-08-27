933f0c694f903ec7779ab2d55e1e553fe7879d7d
Nathan Golba <ngolba@gmail.com>
2018-08-27 13:50:38 -0400

    added restart

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -161,121 +161,261 @@ $('#restartButton').click(function () {
 // I'm using es6 because it's better
 // $('#startButton').click(() => {
 
-    var endGame = () => {
-        $('.playScreen').hide();
-        $('#correct').text(correct);
-        $('#numOfQuestions').text(numOfQuestions)
-        $('.endScreen').show();
-        ending();
+var endGame = () => {
+    $('.playScreen').hide();
+    $('#correct').text(correct);
+    $('#numOfQuestions').text(numOfQuestions)
+    $('.endScreen').show();
+    ending();
+}
+
+var ending = () => {
+    $('.itsasecret').html('<button class="btn btn-dark btn-sm">Hey....</button>')
+    var message = setTimeout(() => {
+        $('.itsasecret').html('<button class="btn btn-dark btn-sm">Hey... click me</button>')
+    }, 3000);
+    message = setTimeout(() => {
+        $('.itsasecret').html('<button class="btn btn-dark btn-sm">Hey... right here</button>')
+    }, 7000);
+}
+var ahhh = ['A', 'H', 'H'];
+var ahhhh = ['A', 'H', 'H', 'H'];
+var ahhhhh = ['A', 'H', 'H', 'H', 'H']
+
+var stop = () => {
+    $('.endScreen').hide();
+    $('.jumbotron').html('<h1 class="display-4">Stop.</h1>');
+    message = setTimeout(() => {
+        $('.display-4').text("Let's play the real game....")
+    }, 3000)
+    $('#realStartButton').text("super awesome actual start button").addClass('float-right').delay(5000).slideDown(500);
+}
+
+$('.itsasecret').click(() => {
+    $('.itsasecret').text('Ah')
+    message = setTimeout(() => {
+        $('.itsasecret').text('AHH')
+    }, 2000)
+    message = setTimeout(() => {
+        var ahhhIterator = setInterval(() => {
+            ahhh.push('H')
+            $('.itsasecret').text(ahhh.join(''))
+            if (ahhh.length >= 150) {
+                clearInterval(ahhhIterator);
+                $('.display-4').text(ahhhh)
+                var ahhhhIterator = setInterval(() => {
+                    ahhhh.push('H')
+                    $('.display-4').text(ahhhh.join(''))
+                    if (ahhhh.length >= 50) {
+                        clearInterval(ahhhhIterator);
+                        $('#restartButton').text(ahhhhh)
+                        var ahhhhhIterator = setInterval(() => {
+                            ahhhhh.push('H')
+                            $('#restartButton').text(ahhhhh.join(''))
+                            if (ahhhhh.length >= 150) {
+                                clearInterval(ahhhhhIterator);
+                                stop();
+                                return;
+                            }
+                        }, 25)
+                    }
+                }, 35)
+            }
+        }, 50)
+
+    }, 5000)
+
+})
+
+var startGame = () => {
+    $('canvas').show();
+    $('.container').hide();
+    theGame();
+}
+
+
+// skip for debugging
+// endGame();
+
+
+// Slow Version/////////////
+
+// ending();
+
+/////////////////////////////
+
+// fast version/////////
+
+// var stopFast = () => {
+//     $('.endScreen').hide();
+//     $('.jumbotron').html('<h1 class="display-4">Stop.</h1>');
+//     message = setTimeout(() => {
+//         $('.display-4').text("Let's play the real game....")
+//     }, 10)
+//     $('#realStartButton').text("super awesome actual start button").addClass('float-right').slideDown(500);
+// }
+
+// stopFast();
+
+/////////////////////
+
+
+$('#realStartButton').click(() => {
+    startGame();
+})
+
+var playingField = document.getElementById('playingField');
+var ctx = playingField.getContext('2d');
+
+var resetPlayingField = () => {
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+
+    ctx.fillRect(0, 0, playingField.width, playingField.height);
+    ctx.strokeRect(0, 0, playingField.width, playingField.height);
+}
+
+var snake = [{
+        x: 250,
+        y: 250
+    },
+    {
+        x: 240,
+        y: 250
+    },
+    {
+        x: 230,
+        y: 250
+    },
+    {
+        x: 220,
+        y: 250
+    },
+    {
+        x: 210,
+        y: 250
+    },
+]
+
+var drawSnakeBits = function (snakeBit) {
+    ctx.fillStyle = 'grey';
+    ctx.strokeStyle = 'black';
+
+    ctx.fillRect(snakeBit.x, snakeBit.y, 10, 10);
+    ctx.strokeRect(snakeBit.x, snakeBit.y, 10, 10);
+}
+
+var drawSnake = () => {
+    snake.forEach(drawSnakeBits)
+}
+
+var speedX = 10;
+var speedY = 0;
+var score = 0;
+
+
+var moveSnake = () => {
+    var snakeHead = {
+        x: snake[0].x + speedX,
+        y: snake[0].y + speedY
+    }
+    snake.unshift(snakeHead);
+    if (snake[0].x === foodX && snake[0].y === foodY) {
+        score++;
+        makeFood()
+    } else {
+        snake.pop();
     }
 
-    var ending = () => {
-        $('.itsasecret').html('<button class="btn btn-dark btn-sm">Hey....</button>')
-        var message = setTimeout(() => {
-            $('.itsasecret').html('<button class="btn btn-dark btn-sm">Hey... click me</button>')
-        }, 3000);
-        message = setTimeout(() => {
-            $('.itsasecret').html('<button class="btn btn-dark btn-sm">Hey... right here</button>')
-        }, 7000);
+}
+
+var generateRandomCords = (max, min) => {
+    return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+
+}
+
+var foodX;
+var foodY;
+
+var makeFood = () => {
+
+    foodX = generateRandomCords(499, 0);
+    foodY = generateRandomCords(499, 0);
+
+
+    snake.forEach(
+        (snakeBit) => {
+            if (snakeBit.x === foodX && snakeBit.y === foodY) {
+                makeFood();
+            }
+        }
+    )
+
+    console.log(foodX + ' ' + foodY)
+
+}
+
+var placeFood = () => {
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'black';
+    ctx.fillRect(foodX, foodY, 10, 10);
+    ctx.strokeRect(foodX, foodY, 10, 10);
+
+}
+
+var butDidYouDieThough = () => {
+    if (snake[0].x < 0 || snake[0].x > 490 || snake[0].y < 0 || snake[0].y > 490) {
+        console.log('topBork')
+        return true;
+
     }
-    var ahhh = ['A', 'H', 'H'];
-    var ahhhh = ['A', 'H', 'H', 'H'];
-    var ahhhhh = ['A', 'H', 'H', 'H', 'H']
-
-    var stop = () => {
-        $('.endScreen').hide();
-        $('.jumbotron').html('<h1 class="display-4">Stop.</h1>');
-        message = setTimeout(() => {
-            $('.display-4').text("Let's play the real game....")
-        }, 3000)
-        $('#realStartButton').text("super awesome actual start button").addClass('float-right').delay(5000).slideDown(500);
+    // shoutout to freeCodeCamp for explaining why the index starts at 4. logic or whatever. 
+    for (var i = 4; i < snake.length; i++) {
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+            console.log('bottomBork');
+            return true;
+        }
     }
+}
 
-    $('.itsasecret').click(() => {
-        $('.itsasecret').text('Ah')
-        message = setTimeout(() => {
-            $('.itsasecret').text('AHH')
-        }, 2000)
-        message = setTimeout(() => {
-            var ahhhIterator = setInterval(() => {
-                ahhh.push('H')
-                $('.itsasecret').text(ahhh.join(''))
-                if (ahhh.length >= 150) {
-                    clearInterval(ahhhIterator);
-                    $('.display-4').text(ahhhh)
-                    var ahhhhIterator = setInterval(() => {
-                        ahhhh.push('H')
-                        $('.display-4').text(ahhhh.join(''))
-                        if (ahhhh.length >= 50) {
-                            clearInterval(ahhhhIterator);
-                            $('#restartButton').text(ahhhhh)
-                            var ahhhhhIterator = setInterval(() => {
-                                ahhhhh.push('H')
-                                $('#restartButton').text(ahhhhh.join(''))
-                                if (ahhhhh.length >= 150) {
-                                    clearInterval(ahhhhhIterator);
-                                    stop();
-                                    return;
-                                }
-                            }, 25)
-                        }
-                    }, 35)
-                }
-            }, 50)
+drawSnake();
+makeFood();
 
-        }, 5000)
+var paused = false;
 
-    })
 
-    var startGame = () => {
-        $('canvas').show();
-        $('.container').hide();
-        theGame();
+document.onkeydown = function (event) {
+    if (event.key === 'ArrowRight' && speedX === 0) {
+        speedX = 10;
+        speedY = 0;
     }
-
-
-    // skip for debugging
-    // endGame();
-
-
-    // Slow Version/////////////
-
-    // ending();
-
-    /////////////////////////////
-
-    // fast version/////////
-
-    // var stopFast = () => {
-    //     $('.endScreen').hide();
-    //     $('.jumbotron').html('<h1 class="display-4">Stop.</h1>');
-    //     message = setTimeout(() => {
-    //         $('.display-4').text("Let's play the real game....")
-    //     }, 10)
-    //     $('#realStartButton').text("super awesome actual start button").addClass('float-right').slideDown(500);
-    // }
-
-    // stopFast();
-
-    /////////////////////
-
-
-    $('#realStartButton').click(() => {
-        startGame();
-    })
-
-    var playingField = document.getElementById('playingField');
-    var ctx = playingField.getContext('2d');
-
-    var resetPlayingField = () => {
-        ctx.fillStyle = 'white';
-        ctx.strokeStyle = 'black';
-
-        ctx.fillRect(0, 0, playingField.width, playingField.height);
-        ctx.strokeRect(0, 0, playingField.width, playingField.height);
+    if (event.key === 'ArrowLeft' && speedX === 0) {
+        speedX = -10;
+        speedY = 0;
+    }
+    if (event.key === 'ArrowUp' && speedY === 0) {
+        speedY = -10;
+        speedX = 0;
+    }
+    if (event.key === 'ArrowDown' && speedY === 0) {
+        speedY = 10;
+        speedX = 0;
     }
 
-    var snake = [{
+    // debugging feature 
+    if (event.key === 'p') {
+        speedY = 0;
+        speedX = 0;
+    }
+    //
+
+}
+
+var resetButtonButForTheSnakeThing = () => {
+    $('.gameOverScreen').hide()
+    $('canvas').show();
+    score = 0;
+    snake = [{
             x: 250,
             y: 250
         },
@@ -297,138 +437,31 @@ $('#restartButton').click(function () {
         },
     ]
 
-    var drawSnakeBits = function (snakeBit) {
-        ctx.fillStyle = 'grey';
-        ctx.strokeStyle = 'black';
-
-        ctx.fillRect(snakeBit.x, snakeBit.y, 10, 10);
-        ctx.strokeRect(snakeBit.x, snakeBit.y, 10, 10);
-    }
-
-    var drawSnake = () => {
-        snake.forEach(drawSnakeBits)
-    }
-
-    var speedX = 10;
-    var speedY = 0;
-    var score = 0;
-
-
-    var moveSnake = () => {
-        var snakeHead = {
-            x: snake[0].x + speedX,
-            y: snake[0].y + speedY
-        }
-        snake.unshift(snakeHead);
-        if (snake[0].x === foodX && snake[0].y === foodY) {
-            score++;
-            makeFood()
-        } else {
-            snake.pop();
-        }
-
-    }
-
-    var generateRandomCords = (max, min) => {
-        return Math.round((Math.random() * (max - min) + min) / 10) * 10;
-
-    }
-
-    var foodX;
-    var foodY;
-
-    var makeFood = () => {
-
-        foodX = generateRandomCords(499, 0);
-        foodY = generateRandomCords(499, 0);
-
-
-        snake.forEach(
-            (snakeBit) => {
-                if (snakeBit.x === foodX && snakeBit.y === foodY) {
-                    makeFood();
-                }
-            }
-        )
-
-        console.log(foodX + ' ' + foodY)
-
-    }
-
-    var placeFood = () => {
-        ctx.fillStyle = 'red';
-        ctx.strokeStyle = 'black';
-        ctx.fillRect(foodX, foodY, 10, 10);
-        ctx.strokeRect(foodX, foodY, 10, 10);
-
-    }
-
-    var butDidYouDieThough = () => {
-        if (snake[0].x < 0 || snake[0].x > 490 || snake[0].y < 0 || snake[0].y > 490) {
-            console.log('topBork')
-            return true;
-
-        }
-        // shoutout to freeCodeCamp for explaining why the index starts at 4. logic or whatever. 
-        for (var i = 4; i < snake.length; i++) {
-            if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
-                console.log('bottomBork');
-                return true;
-            }
-        }
-    }
-
     drawSnake();
     makeFood();
+    theGame();
+}
 
-    var paused = false;
+var theGame = () => {
 
 
-    document.onkeydown = function (event) {
-        if (event.key === 'ArrowRight' && speedX === 0) {
-            speedX = 10;
-            speedY = 0;
+
+    var gameRunning = setInterval(function () {
+        resetPlayingField();
+        moveSnake();
+        drawSnake();
+        placeFood();
+        if (butDidYouDieThough()) {
+            console.log('borked');
+            clearInterval(gameRunning);
+            $('canvas').hide();
+            $('.gameOverScreen').show();
+            $('#score').text("Score: " + score);
+            
         }
-        if (event.key === 'ArrowLeft' && speedX === 0) {
-            speedX = -10;
-            speedY = 0;
-        }
-        if (event.key === 'ArrowUp' && speedY === 0) {
-            speedY = -10;
-            speedX = 0;
-        }
-        if (event.key === 'ArrowDown' && speedY === 0) {
-            speedY = 10;
-            speedX = 0;
-        }
+    }, 100);
+}
 
-        // debugging feature 
-        if (event.key === 'p') {
-            speedY = 0;
-            speedX = 0;
-        }
-        //
-
-    }
-
-    var theGame = () => {
-
-
-
-        var gameRunning = setInterval(function () {
-            resetPlayingField();
-            moveSnake();
-            drawSnake();
-            placeFood();
-            if (butDidYouDieThough()) {
-                console.log('borked');
-                clearInterval(gameRunning);
-                $('canvas').hide();
-                $('.gameOverScreen').show();
-                $('#score').text("Score: " + score);
-            }
-        }, 100);
-    }
-
-
-    
+$('#reRestart').click(() => {
+    resetButtonButForTheSnakeThing()}
+);
