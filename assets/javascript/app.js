d9953962f3199ae4518bad527c6aa6a4cf345662
Nathan Golba <ngolba@gmail.com>
2018-08-27 13:20:08 -0400

    8_27 Mega Build

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,165 +1,165 @@
-// var done = false;
-// var timerRunning = false;
-// var currentQuestionNumber = 0;
-// var answerSubmitted = false;
-// var numOfQuestions = 5;
-// var time = 30;
-// var gameStarted = false;
-// var question1 = new Question('In what organ of the human body would you find the cochlea?', 'eye', 'nose', 'heart', 'ear', 3);
-// var question2 = new Question('Something that is described as "vespine" resembles what type of insect?', 'dragonfly', 'wasp', 'beatle', 'centipede', 1)
-// var question3 = new Question('What "-itis" is an inflammation of the liver?', 'colitis', 'gastroenteritis', 'hepatitis', 'encephalitis', 2);
-// var question4 = new Question('On average, a dragonfly eats how many mosquitoes per day?', '100', '300', '150', '50', 1);
-// var question5 = new Question('"Coulrophobia" is the fear of what?', 'swimming pools', 'bears', 'rainbows', 'clowns', 3);
-// var questionArray = [question1, question2, question3, question4, question5];
-// var currentQuestion = questionArray[currentQuestionNumber];
-// var correct = 0;
-// var incorrect = 0;
+var done = false;
+var timerRunning = false;
+var currentQuestionNumber = 0;
+var answerSubmitted = false;
+var numOfQuestions = 5;
+var time = 30;
+var gameStarted = false;
+var question1 = new Question('In what organ of the human body would you find the cochlea?', 'eye', 'nose', 'heart', 'ear', 3);
+var question2 = new Question('Something that is described as "vespine" resembles what type of insect?', 'dragonfly', 'wasp', 'beatle', 'centipede', 1)
+var question3 = new Question('What "-itis" is an inflammation of the liver?', 'colitis', 'gastroenteritis', 'hepatitis', 'encephalitis', 2);
+var question4 = new Question('On average, a dragonfly eats how many mosquitoes per day?', '100', '300', '150', '50', 1);
+var question5 = new Question('"Coulrophobia" is the fear of what?', 'swimming pools', 'bears', 'rainbows', 'clowns', 3);
+var questionArray = [question1, question2, question3, question4, question5];
+var currentQuestion = questionArray[currentQuestionNumber];
+var correct = 0;
+var incorrect = 0;
 
-// // Constructors are a thing ... 
+// Constructors are a thing ... 
 
-// function Question(question, answer1, answer2, answer3, answer4, correctAnswerIndex) {
-//     this.question = question;
-//     this.answer1 = answer1;
-//     this.answer2 = answer2;
-//     this.answer3 = answer3;
-//     this.answer4 = answer4;
-//     this.correctAnswerIndex = correctAnswerIndex;
-//     this.answerArray = [answer1, answer2, answer3, answer4]
-// }
-
-
-
-// var nextQuestion = function () {
-//     currentQuestionNumber++
-//     setupQuestion(currentQuestionNumber);
-// }
-
-
-// var setupQuestion = function (questionArrayNumber) {
-//     currentQuestion = questionArray[questionArrayNumber];
-//     $('#question').text(currentQuestion.question);
-//     $('#choiceOne').text(currentQuestion.answer1);
-//     $('#choiceTwo').text(currentQuestion.answer2);
-//     $('#choiceThree').text(currentQuestion.answer3);
-//     $('#choiceFour').text(currentQuestion.answer4);
-//     answerSubmitted = false;
-// }
-
-// $('.answerChoice').click(function () {
-//     $(this).addClass('userChoice border');
-//     $(this).siblings().removeClass('userChoice border');
-//     $('#submitButton').show();
-// })
-
-
-// $('#submitButton').click(function () {
-//     $(this).hide()
-//     answer = $('.userChoice').text();
-//     console.log(answer);
-
-//     if (currentQuestion.answerArray.indexOf(answer) === currentQuestion.correctAnswerIndex) {
-//         console.log("correct")
-//         correct++;
-//     } else {
-//         incorrect++;
-//     }
-//     if (currentQuestionNumber === numOfQuestions - 1) {
-//         return answerSubmitted = true;
-//     } else {
-//         nextQuestion();
-//         return answerSubmitted = true;
-
-//     }
-// })
-
-// var endGame = function () {
-//     $('.playScreen').hide();
-//     $('#correct').text(correct);
-//     $('#numOfQuestions').text(numOfQuestions)
-//     $('.endScreen').show();
-//     ending();
-// }
-
-// var timer = function (timeAlloted, numOfQuestions) {
-//     gameStarted = true;
-//     // Prevents multiple iterations of timer at same time
-//     if (timerRunning == true) {
-//         return;
-//     }
-//     //
-//     else {
-//         timerRunning = true;
-//         var time = timeAlloted;
-//         var i = 0;
-//         var countdown = setInterval(function () {
-//             time--;
-//             $('#timer').text(time);
-//             if (time == 0 && i < numOfQuestions) {
-//                 time = timeAlloted;
-//                 done = true;
-//                 i++;
-
-//             } else if ((i === numOfQuestions - 1 && answerSubmitted === true) || (i === numOfQuestions - 1 && time === 0)) {
-//                 timerRunning = false;
-//                 time = 0;
-//                 clearInterval(countdown);
-//                 endGame();
-//                 return;
-//             } else if (answerSubmitted === true) {
-//                 time = timeAlloted;
-//                 i++;
-//                 answerSubmitted = false;
-
-//             }
+function Question(question, answer1, answer2, answer3, answer4, correctAnswerIndex) {
+    this.question = question;
+    this.answer1 = answer1;
+    this.answer2 = answer2;
+    this.answer3 = answer3;
+    this.answer4 = answer4;
+    this.correctAnswerIndex = correctAnswerIndex;
+    this.answerArray = [answer1, answer2, answer3, answer4]
+}
 
 
 
-//         }, 1000)
+var nextQuestion = function () {
+    currentQuestionNumber++
+    setupQuestion(currentQuestionNumber);
+}
 
-//     }
-//     return;
-// }
+
+var setupQuestion = function (questionArrayNumber) {
+    currentQuestion = questionArray[questionArrayNumber];
+    $('#question').text(currentQuestion.question);
+    $('#choiceOne').text(currentQuestion.answer1);
+    $('#choiceTwo').text(currentQuestion.answer2);
+    $('#choiceThree').text(currentQuestion.answer3);
+    $('#choiceFour').text(currentQuestion.answer4);
+    answerSubmitted = false;
+}
+
+$('.answerChoice').click(function () {
+    $(this).addClass('userChoice border');
+    $(this).siblings().removeClass('userChoice border');
+    $('#submitButton').show();
+})
 
 
-// $('#startButton').click(function () {
-//     $(this).hide();
-//     timer(30, 5);
-//     currentQuestionNumber = 0;
+$('#submitButton').click(function () {
+    $(this).hide()
+    answer = $('.userChoice').text();
+    console.log(answer);
 
-//     if (gameStarted === true) {
-//         setupQuestion(currentQuestionNumber);
-//     }
-// });
+    if (currentQuestion.answerArray.indexOf(answer) === currentQuestion.correctAnswerIndex) {
+        console.log("correct")
+        correct++;
+    } else {
+        incorrect++;
+    }
+    if (currentQuestionNumber === numOfQuestions - 1) {
+        return answerSubmitted = true;
+    } else {
+        nextQuestion();
+        return answerSubmitted = true;
 
-// $('#restartButton').click(function () {
-//      done = false;
-//      timerRunning = false;
-//      currentQuestionNumber = 0;
-//      answerSubmitted = false;
-//      numOfQuestions = 5;
-//      time = 30;
-//      gameStarted = false;
-//      questionArray = [question1, question2, question3, question4, question5];
-//      currentQuestion = questionArray[currentQuestionNumber];
-//      correct = 0;
-//      incorrect = 0;
+    }
+})
 
-//      $('.endScreen').hide();
-//      $('.playScreen').show();
-//      timer(30, 5);
-//      currentQuestionNumber = 0;
+var endGame = function () {
+    $('.playScreen').hide();
+    $('#correct').text(correct);
+    $('#numOfQuestions').text(numOfQuestions)
+    $('.endScreen').show();
+    ending();
+}
 
-//      if (gameStarted === true) {
-//          setupQuestion(currentQuestionNumber);
-//      }
-// })
+var timer = function (timeAlloted, numOfQuestions) {
+    gameStarted = true;
+    // Prevents multiple iterations of timer at same time
+    if (timerRunning == true) {
+        return;
+    }
+    //
+    else {
+        timerRunning = true;
+        var time = timeAlloted;
+        var i = 0;
+        var countdown = setInterval(function () {
+            time--;
+            $('#timer').text(time);
+            if (time == 0 && i < numOfQuestions) {
+                time = timeAlloted;
+                done = true;
+                i++;
+
+            } else if ((i === numOfQuestions - 1 && answerSubmitted === true) || (i === numOfQuestions - 1 && time === 0)) {
+                timerRunning = false;
+                time = 0;
+                clearInterval(countdown);
+                endGame();
+                return;
+            } else if (answerSubmitted === true) {
+                time = timeAlloted;
+                i++;
+                answerSubmitted = false;
+
+            }
+
+
+
+        }, 1000)
+
+    }
+    return;
+}
+
+
+$('#startButton').click(function () {
+    $(this).hide();
+    timer(30, 5);
+    currentQuestionNumber = 0;
+
+    if (gameStarted === true) {
+        setupQuestion(currentQuestionNumber);
+    }
+});
+
+$('#restartButton').click(function () {
+    done = false;
+    timerRunning = false;
+    currentQuestionNumber = 0;
+    answerSubmitted = false;
+    numOfQuestions = 5;
+    time = 30;
+    gameStarted = false;
+    questionArray = [question1, question2, question3, question4, question5];
+    currentQuestion = questionArray[currentQuestionNumber];
+    correct = 0;
+    incorrect = 0;
+
+    $('.endScreen').hide();
+    $('.playScreen').show();
+    timer(30, 5);
+    currentQuestionNumber = 0;
+
+    if (gameStarted === true) {
+        setupQuestion(currentQuestionNumber);
+    }
+})
 
 
 
 // That was easy...
 //alt for testing 
 // I'm using es6 because it's better
-$('#startButton').click(() => {
+// $('#startButton').click(() => {
 
     var endGame = () => {
         $('.playScreen').hide();
@@ -202,10 +202,10 @@ $('#startButton').click(() => {
                 $('.itsasecret').text(ahhh.join(''))
                 if (ahhh.length >= 150) {
                     clearInterval(ahhhIterator);
-                    $('h1').text(ahhhh)
+                    $('.display-4').text(ahhhh)
                     var ahhhhIterator = setInterval(() => {
                         ahhhh.push('H')
-                        $('h1').text(ahhhh.join(''))
+                        $('.display-4').text(ahhhh.join(''))
                         if (ahhhh.length >= 50) {
                             clearInterval(ahhhhIterator);
                             $('#restartButton').text(ahhhhh)
@@ -230,9 +230,13 @@ $('#startButton').click(() => {
     var startGame = () => {
         $('canvas').show();
         $('.container').hide();
+        theGame();
     }
 
-    endGame();
+
+    // skip for debugging
+    // endGame();
+
 
     // Slow Version/////////////
 
@@ -242,16 +246,16 @@ $('#startButton').click(() => {
 
     // fast version/////////
 
-    var stopFast = () => {
-        $('.endScreen').hide();
-        $('.jumbotron').html('<h1 class="display-4">Stop.</h1>');
-        message = setTimeout(() => {
-            $('.display-4').text("Let's play the real game....")
-        }, 10)
-        $('#realStartButton').text("super awesome actual start button").addClass('float-right').slideDown(500);
-    }
+    // var stopFast = () => {
+    //     $('.endScreen').hide();
+    //     $('.jumbotron').html('<h1 class="display-4">Stop.</h1>');
+    //     message = setTimeout(() => {
+    //         $('.display-4').text("Let's play the real game....")
+    //     }, 10)
+    //     $('#realStartButton').text("super awesome actual start button").addClass('float-right').slideDown(500);
+    // }
 
-    stopFast();
+    // stopFast();
 
     /////////////////////
 
@@ -305,8 +309,9 @@ $('#startButton').click(() => {
         snake.forEach(drawSnakeBits)
     }
 
-    var speedX = 0;
+    var speedX = 10;
     var speedY = 0;
+    var score = 0;
 
 
     var moveSnake = () => {
@@ -315,9 +320,12 @@ $('#startButton').click(() => {
             y: snake[0].y + speedY
         }
         snake.unshift(snakeHead);
-
-
-        snake.pop();
+        if (snake[0].x === foodX && snake[0].y === foodY) {
+            score++;
+            makeFood()
+        } else {
+            snake.pop();
+        }
 
     }
 
@@ -331,8 +339,8 @@ $('#startButton').click(() => {
 
     var makeFood = () => {
 
-        foodX = generateRandomCords(500, 0);
-        foodY = generateRandomCords(500, 0);
+        foodX = generateRandomCords(499, 0);
+        foodY = generateRandomCords(499, 0);
 
 
         snake.forEach(
@@ -352,7 +360,22 @@ $('#startButton').click(() => {
         ctx.strokeStyle = 'black';
         ctx.fillRect(foodX, foodY, 10, 10);
         ctx.strokeRect(foodX, foodY, 10, 10);
-        
+
+    }
+
+    var butDidYouDieThough = () => {
+        if (snake[0].x < 0 || snake[0].x > 490 || snake[0].y < 0 || snake[0].y > 490) {
+            console.log('topBork')
+            return true;
+
+        }
+        // shoutout to freeCodeCamp for explaining why the index starts at 4. logic or whatever. 
+        for (var i = 4; i < snake.length; i++) {
+            if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+                console.log('bottomBork');
+                return true;
+            }
+        }
     }
 
     drawSnake();
@@ -378,19 +401,34 @@ $('#startButton').click(() => {
             speedY = 10;
             speedX = 0;
         }
-        if(event.key === 'p') {
+
+        // debugging feature 
+        if (event.key === 'p') {
             speedY = 0;
             speedX = 0;
         }
+        //
 
     }
 
-    setInterval(function () {
-        resetPlayingField();
-        moveSnake();
-        drawSnake();
-        placeFood();
-    }, 100);
+    var theGame = () => {
 
 
-});
+
+        var gameRunning = setInterval(function () {
+            resetPlayingField();
+            moveSnake();
+            drawSnake();
+            placeFood();
+            if (butDidYouDieThough()) {
+                console.log('borked');
+                clearInterval(gameRunning);
+                $('canvas').hide();
+                $('.gameOverScreen').show();
+                $('#score').text("Score: " + score);
+            }
+        }, 100);
+    }
+
+
+    
